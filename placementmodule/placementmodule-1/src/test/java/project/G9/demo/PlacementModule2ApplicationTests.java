a3ae6a3da68590a0426d705de46112cb2238cd63
SHAIKRIZWANA16 <155900035+SHAIKRIZWANA16@users.noreply.github.com>
2024-08-09 22:18:55 +0530

    Add files via upload

---
+++ placementmodule/placementmodule-1/src/test/java/project/G9/demo/PlacementModule2ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.G9.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacementModule2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
